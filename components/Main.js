aaa60cc3484659ea798e59b911789d4295541ed9
Douglas Pires Vilela da Silva <douglas.pires@live.com>
2018-05-04 17:33:28 -0300

    changing compileSdkVersion to 27

---
+++ components/Main.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { View, StyleSheet } from 'react-native'
+import Video from './video/Video'
+
+export default class Main extends Component {
+
+  render () {
+    return (
+      <View style={ styles.container }>
+        <Video />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
---
+++ components/video/Video.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+
+export default class Video extends Component {
+  render () {
+    return (
+      <Text>This is Video</Text>
+    )
+  }
+}
